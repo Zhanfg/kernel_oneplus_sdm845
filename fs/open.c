4b2923fbbad50800363a0b930ca6f06184cf575d
abk-builder <abk-builder@users.noreply.github.com>
2026-06-08 17:18:16 +0800

    Fix open.c missing susfs_def.h include + Kbuild KSU_VERSION_FULL quoting

--- fs/open.c
+++ fs/open.c
@@ -1097,6 +1097,7 @@ struct file *file_open_root(struct dentry *dentry, struct vfsmount *mnt,
 EXPORT_SYMBOL(file_open_root);
 
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
+#include <linux/susfs_def.h>
 extern struct filename *susfs_open_redirect_spoof_do_sys_openat(struct inode *inode);
 #endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 
