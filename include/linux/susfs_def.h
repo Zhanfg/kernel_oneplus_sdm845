0831e36ee925138d0125496121a2e984ac16d95e
abk-builder <abk-builder@users.noreply.github.com>
2026-06-08 17:04:12 +0800

    Add missing DEFAULT_SUS_MNT_ID defines for SuSFS mount handling

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -47,6 +47,9 @@
 #define TRY_UMOUNT_DETACH 1 /* used by susfs_try_umount() */
 
 #define VFSMOUNT_MNT_FLAGS_KSU_UNSHARED_MNT 0x80000000 /* used for mounts that are unshared by ksu process */
+#define DEFAULT_SUS_MNT_ID 100000 /* used by mount->mnt_id for sus mounts */
+#define DEFAULT_SUS_MNT_ID_FOR_KSU_PROC_UNSHARE 1000000 /* used by vfsmount->susfs_mnt_id_backup */
+#define DEFAULT_SUS_MNT_GROUP_ID 1000 /* used by mount->mnt_group_id for sus mounts */
 #define DEFAULT_KSU_MNT_ID 2000000000 /* used for mounts created or single cloned by ksu process */
 #define DEFAULT_KSU_MNT_GROUP_ID 200000 /* used by mount->mnt_group_id */
 
